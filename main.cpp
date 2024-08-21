53176697104624ca1f3fb5fdc574fe5e20a2d895
Dương <142103480+HaiDuong2000@users.noreply.github.com>
2024-08-22 00:22:29 +0700

    Update main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int sum (int a, int b)
+{
+	return a+b;
+}
+
+int main()
+{
+
+    cout << "Hello World"  << endl;
+	int res = sum(2, 2);
+	cout << res << endl;
+
+    return 0;
+}
